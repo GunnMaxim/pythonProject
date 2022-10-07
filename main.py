4b44c931ad00bc4109328a95da78b16fb4a686f4
GunnMaxim <wiut00013841@gmail.com>
2022-10-07 13:51:29 +0500

    init : commited

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+import time
+n = int(input("Input positive number for a 'CountDown' or negative number for a 'CountUp' "))
+
+while n>0:
+    time.sleep(1)
+    n -= 1
+    print(n)
+if (n == 0):
+    print("Breakout!")
+while n<0:
+    time.sleep(1)
+    n += 1
+    print(n)
+if (n == 0):
+    print("Breakout!")
